ce525a62fc37c57f7256990ff20d058fe6994f04
lucasgualampa <g.brianlucas1993@gmail.com>
2021-04-28 18:32:21 -0300

    added emojis

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+var emoji = require('node-emoji')
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Working in a beautiful portfolio, stay tuned {emoji.get(':tv:')}</h3>
       </header>
     </div>
   );
